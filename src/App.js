8bec4c1cfc53fa104ae131e5919c2450ae72d99c
SB-0111 <samuelbejarano0111@gmail.com>
2022-10-28 22:13:20 -0500

    cambio2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <p>Pasos para conseguir la Green Card</p>
         <p>Enamorar a una gringa</p>
-        <p>abandonar a la gringa</p>
+        <p>Abandonar a la gringa</p>
       </header>
     </div>
   );
